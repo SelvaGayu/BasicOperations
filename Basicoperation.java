33c202d6f98c936d315a6491da9b327a7e6c6c28
SelvaGayu <117286743+SelvaGayu@users.noreply.github.com>
2023-01-12 17:24:21 +0530

    Initial Commit

---
+++ Basicoperation.java
@@ -0,0 +1,13 @@
+package com.mathoperation;
+
+public class Basicoperation {
+
+	public static void main(String[] args) {
+		System.out.println("Basic operations");
+		System.out.println("Addition");
+		int a=10;
+		int b=5;
+		System.out.println("sum of 2nos: "+(a+b));
+	}
+
+}
